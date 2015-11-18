7b9b9a08ebd73d842041bf3f515cbe7c70d8b04b
BingKui <178974662@qq.com>
2015-11-18 17:32:07 +0800

    添加基础组件

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+﻿module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		concat: {
+			cssbuild: {
+				src: ["css/*.css"],
+				dest: "dest/<%= pkg.name %>.css"
+			}
+		}, 	
+		cssmin: {
+			options: {
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+				beautify: {
+					ascii_only: true
+				}
+			},
+			build: {
+				files: [{
+					"dest/<%= pkg.name %>.min.css": ['<%= concat.cssbuild.dest %>']
+				}]
+			}
+		},
+	});
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.registerTask('default', ['concat', 'cssmin']);
+}
